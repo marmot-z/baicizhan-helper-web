d4d1befd9bb42f58a58afca05e38cf216169824b
zhangxunwei <zhangxunwei@dxy.cn>
2025-08-13 18:27:09 +0800

    feat: 开发错误提示功能

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { RouterProvider } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 import { router } from './router';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <RouterProvider router={router} />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+          style: {
+            background: '#363636',
+            color: '#fff',
+          },
+        }}
+      />
     </div>
   );
 }
